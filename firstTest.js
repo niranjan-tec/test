15f28c3e4809b7bedf4c2099ae23cb346925e811
niranjan12apl1998 <niranjankumar2252@gmail.com>
2022-12-17 11:38:17 +0530

    changed code of firstText file

--- firstTest.js
+++ firstTest.js
@@ -1,1 +1,9 @@
-console.log("hey guys hello")
+function sweets(n) {
+    if(n==2){
+        console.log("I like this fruits")
+    }
+    else{
+        console.log("I don't like this fruits")
+    }
+}
+sweets(1);
